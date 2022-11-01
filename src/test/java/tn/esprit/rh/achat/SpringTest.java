cf42c75bfb1f210aae49b9c91f291ba72ed9b60a
khalillan <landoulsi.khalil@esprit.tn>
2022-11-01 23:14:37 +0100

    test

---
+++ src/test/java/tn/esprit/rh/achat/SpringTest.java
@@ -0,0 +1,38 @@
+package tn.esprit.rh.achat;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Fournisseur;
+import tn.esprit.rh.achat.repositories.FournisseurRepository;
+import tn.esprit.rh.achat.services.FournisseurServiceImpl;
+
+import java.util.Optional;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+public class SpringTest {
+
+    @Mock
+    FournisseurRepository fournisseurRepository;
+
+    @InjectMocks
+    FournisseurServiceImpl fournisseurService;
+
+    Fournisseur fournisseur = new Fournisseur(1,"ff","ff",null,null,null,null);
+
+    @Test
+    public void retriveFournissur(){
+        Mockito.when(fournisseurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(fournisseur));
+        Fournisseur f = fournisseurService.retrieveFournisseur(1);
+        log.info("get ==> " + f.toString());
+    }
+
+}
